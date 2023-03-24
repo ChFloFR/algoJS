798f2897eebe98612d00ef3495d22de5d8af7d59
FlorentN <floneuville@gmail.com>
2023-03-24 19:40:21 +0100

    exo 3 regex

--- Algorithmes/1. Fichiers de départ/3.Vowels/script.js
+++ Algorithmes/1. Fichiers de départ/3.Vowels/script.js
@@ -1,17 +1,14 @@
 function getVowelsNumber(txt) {
-
+  const regexVoyelles = /([a,e,i,o,u,y]|)+/gi;
 }
-
 
 /* ÉNONCÉ 📚 */
 
-
 /* Créez un programme qui retourne le nombre de voyelles dans une chaîne de caractères. */
-
 
 /* Tests à passer 🧪 */
 
-console.log(getVowelsNumber("jdhqgdqsghdakzejamazemlqksd"));               // 5
-console.log(getVowelsNumber("Lorem ipsum dolor sit amet consectetur."));   // 13
-console.log(getVowelsNumber("L’imagination gouverne le monde."));          // 13
-console.log(getVowelsNumber("zzzZZZzz"));                                  // 0
+console.log(getVowelsNumber("jdhqgdqsghdakzejamazemlqksd")); // 5
+console.log(getVowelsNumber("Lorem ipsum dolor sit amet consectetur.")); // 13
+console.log(getVowelsNumber("L’imagination gouverne le monde.")); // 13
+console.log(getVowelsNumber("zzzZZZzz")); // 0
